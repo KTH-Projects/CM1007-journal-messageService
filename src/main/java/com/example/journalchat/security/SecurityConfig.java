8d17e2e841ca049dc99c18583cd1dec11636c323
tattack99 <62747925+tattack99@users.noreply.github.com>
2023-12-29 09:07:02 +0100

    added keycloak

---
+++ src/main/java/com/example/journalchat/security/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.journalchat.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> {
+            authorize.anyRequest().permitAll();
+        });
+
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+        http.sessionManagement(
+                t->{
+                    t.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                }
+        );
+        return http.build();
+    }
+
+    @Bean
+    public DefaultMethodSecurityExpressionHandler msecurity(){
+        DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler = new DefaultMethodSecurityExpressionHandler();
+        defaultMethodSecurityExpressionHandler.setDefaultRolePrefix("");
+        return defaultMethodSecurityExpressionHandler;
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+            // Get realm_access from the JWT where the roles are
+            Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                ((List<String>) realmAccess.get("roles")).forEach(
+
+                        role -> {
+                            System.out.println(role);
+                            grantedAuthorities.add(new SimpleGrantedAuthority(role));}
+                );
+            }
+            return grantedAuthorities;
+        });
+        return jwtAuthenticationConverter;
+    }
+
+
+
+
+}
--- src/main/java/com/example/journalchat/view/MessageController.java
+++ src/main/java/com/example/journalchat/view/MessageController.java
@@ -5,6 +5,7 @@ import com.example.journalchat.core.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +17,21 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
+    @PreAuthorize("hasRole('user')")
     @PostMapping("/send")
     public ResponseEntity<MessageDTO> sendMessage(@RequestBody MessageDTO message){
         MessageDTO sentMessage = messageService.sendMessage(message.getFromId(), message.getToId(), message.getMessage());
         return new ResponseEntity<>(sentMessage, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('user')")
     @GetMapping("")
     public ResponseEntity<List<MessageDTO>> getMessagesFromId(@RequestParam String fromId) {
         List<MessageDTO> conversation = messageService.getMessagesFromId(fromId);
         return new ResponseEntity<>(conversation, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('user')")
     @GetMapping("/chat")
     public ResponseEntity<List<MessageDTO>> getMessagesFromIdAndToId(@RequestParam String fromId, @RequestParam String toId) {
         List<MessageDTO> conversation = messageService.getMessagesFromIdAndToId(fromId,toId);
